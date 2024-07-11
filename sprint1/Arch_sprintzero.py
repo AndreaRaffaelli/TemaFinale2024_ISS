c5cd5db28fa6d00f637267458b6808f3f98cacb5
EdoardoDd <edoardo.dedivitiis@studio.unibo.it>
2024-07-11 14:42:46 +0200

    commit aggiornamenti sprint uno

---
+++ sprint1/Arch_sprintzero.py
@@ -0,0 +1,35 @@
+### conda install diagrams
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+evattr = {
+    'color': 'darkgreen',
+    'style': 'dotted'
+}
+with Diagram('sprintzeroArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctxservicearea', graph_attr=nodeattr):
+          wis=Custom('wis','./qakicons/symActorWithobjSmall.png')
+          oprobot=Custom('oprobot','./qakicons/symActorSmall.png')
+          incinerator=Custom('incinerator','./qakicons/symActorSmall.png')
+     sys >> Edge( label='burnEnd', **evattr, decorate='true', fontcolor='darkgreen') >> wis
+     sys >> Edge( label='burnEnd', **evattr, decorate='true', fontcolor='darkgreen') >> oprobot
+     incinerator >> Edge( label='burnEnd', **eventedgeattr, decorate='true', fontcolor='red') >> sys
+diag
---
+++ sprint1/sprintzero.pl
@@ -0,0 +1,12 @@
+%====================================================================================
+% sprintzero description   
+%====================================================================================
+event( burnEnd, burnEnd(X) ).
+%====================================================================================
+context(ctxservicearea, "localhost",  "TCP", "8001").
+ qactor( wis, ctxservicearea, "it.unibo.wis.Wis").
+ static(wis).
+  qactor( oprobot, ctxservicearea, "it.unibo.oprobot.Oprobot").
+ static(oprobot).
+  qactor( incinerator, ctxservicearea, "it.unibo.incinerator.Incinerator").
+ static(incinerator).
