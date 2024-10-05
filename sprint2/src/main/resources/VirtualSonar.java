86f0f40349d20bb16f20ca135e6acd1627e5d08b
EdoardoDd <edoardo.dedivitiis@studio.unibo.it>
2024-10-05 21:32:12 +0200

    VirtualSonar

--- sprint2/src/main/resources/VirtualSonar.java
+++ sprint2/src/main/resources/VirtualSonar.java
@@ -5,24 +5,29 @@ import unibo.basicomm23.interfaces.Interaction;
 import unibo.basicomm23.msg.ProtocolType;
 import unibo.basicomm23.utils.CommUtils;
 import unibo.basicomm23.utils.ConnectionFactory;
+import java.util.Random;
 
 public class VirtualSonar implements IVirtualSonar{
-	private String vitualRobotIp;
-	private ActorBasic owner;
-	
-	private static final String ADDRESS = "127.0.0.1"; // Indirizzo del receiver
-	private static final String PORT = "8021"; // Porta (modificare secondo necessità)
+
 	private static final ProtocolType PROTOCOL = ProtocolType.tcp; // Protocollo da utilizzare
 	private Interaction connSupport;
 	
-	public VirtualSonar(String vitualRobotIp, ActorBasic owner) {
-		this.owner = owner;
-		this.vitualRobotIp = vitualRobotIp;
-		this.connSupport = ConnectionFactory.createClientSupport(PROTOCOL, ADDRESS, PORT);
+	public VirtualSonar(String address, String port) {
+		this.connSupport = ConnectionFactory.createClientSupport(PROTOCOL, address, port);
+
 	}
 	
-	public int read() {
-		return 1;
+	public boolean read() {
+		Random random = new Random();
+        int randomNumber = random.nextInt(10) + 1; // genera un numero tra 1 e 10
+		var msg = CommUtils.buildDispatch("SONAR", "info", "info("+randomNumber+")", "test_observer");			
+		try {
+			connSupport.forward(msg);
+		} catch (Exception e) {
+			CommUtils.outred("Fatal error sonar");
+			return false;
+		}
+		return true;
 	}
 
 	
