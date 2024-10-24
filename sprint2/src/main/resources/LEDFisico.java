04213e401aa1daa2190b2c4aa87ee2eaaad04e4c
EdoardoDd <edoardo.dedivitiis@studio.unibo.it>
2024-10-24 15:49:10 +0200

    Led Fisico

--- sprint2/src/main/resources/LEDFisico.java
+++ sprint2/src/main/resources/LEDFisico.java
@@ -1,36 +1,58 @@
 package main.resources;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
-import unibo.basicomm23.utils.ConnectionFactory;
+public class LEDFisico implements ILED {
+    private Process p;
+    private BufferedWriter writer;
 
-public class LEDFisico implements ILED{
-	private  Process p;
-	
-	public LEDFisico() {}
+    public LEDFisico() {
+        try {
+            // Avvia il processo Python che esegue LedDevice.py
+            this.p = Runtime.getRuntime().exec("python3 src/main/resources/Led.py");
+            this.writer = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public boolean turnOn() {
+        return sendCommand("on");
+    }
 
-	@Override
-	public boolean turnOn() {
-		try {
-			this.p       = Runtime.getRuntime().exec("python3 src/main/resources/sonar.py");
-		} catch (IOException e) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean turnOff() {
+        return sendCommand("off");
+    }
 
-	@Override
-	public boolean turnOff() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean turnBlink() {
+        return sendCommand("blink");
+    }
 
-	@Override
-	public boolean turnBlink() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
+    private boolean sendCommand(String command) {
+        try {
+            // Invia il comando al processo Python tramite stdin
+            writer.write(command);
+            writer.newLine();  // Aggiunge una nuova riga per inviare il comando correttamente
+            writer.flush();    // Assicura che il comando venga inviato subito
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
+    // Metodo per chiudere correttamente il processo Python e liberare risorse
+    public void close() {
+        try {
+            writer.close();
+            p.destroy();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
