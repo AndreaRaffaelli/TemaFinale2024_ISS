910e8c7b7cc4c491fe9ca39968d24bed097f2dae
EdoardoDd <edoardo.dedivitiis@studio.unibo.it>
2024-11-22 19:08:48 +0100

    Facade rotta

---
+++ sprint-gui/src/main/java/prog_iss/spring3/FacadeController.java
@@ -0,0 +1,58 @@
+package prog_iss.spring3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import unibo.basicomm23.utils.CommUtils;
+ 
+
+ 
+
+@Controller
+public class FacadeController {
+    String protocol="tcp";
+    //@Value("${robot23.webcamip}")
+    String webcamip;
+    @Value("${wis.wisip}")
+    String wisip="wis";
+
+    @Value("${spring.application.name}")
+    String appNameOld;  //vedi application.properties
+     
+    protected String mainPage = "Fcd24SGui"; //TODO: "WebRobot24Gui";  
+
+    public FacadeController(){
+        CommUtils.outgreen (" --- FacadeController | STARTS " );
+        new FacadeBuilder( ) ;
+    }
+ 
+
+    protected String buildThePage(Model viewmodel) {
+        setConfigParams(viewmodel);
+        return mainPage;
+    }
+    protected void setConfigParams(Model viewmodel){
+ 
+    }
+    @GetMapping("/")
+    public String homePage(Model viewmodel) {
+        //CommUtils.outcyan("FacadeController homePage appNameOld=" + appNameOld);
+        viewmodel.addAttribute("appname", ApplSystemInfo.appName);
+        String dir = System.getProperty("user.dir");
+        CommUtils.outgreen (" --- FacadeController | entry dir= "+dir  );
+        return buildThePage(viewmodel); //"qakFacadeGUI";
+    }
+    
+    @PostMapping("/addrp")
+    public String addrp(Model viewmodel, @RequestParam String ipaddr  ){
+        wisip = ipaddr;
+        System.out.println("ServiceFacadeController | setrobotip:" + ipaddr );
+        viewmodel.addAttribute("wisip", wisip);
+        Utils.connectWithRobotUsingTcp(ipaddr);
+        return buildThePage(viewmodel);
+    }
+     
+}
