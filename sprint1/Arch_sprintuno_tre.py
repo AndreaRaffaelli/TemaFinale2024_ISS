85a72810875757b3bd980b502306120190f278ae
AndreaRaffaelli <andrea@localhost.localdomain>
2024-12-13 17:50:48 +0100

    refactoring, moving to docker compose, changed ips,changed global test

---
+++ sprint1/Arch_sprintuno_tre.py
@@ -0,0 +1,43 @@
+### conda install diagrams
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+evattr = {
+    'color': 'darkgreen',
+    'style': 'dotted'
+}
+with Diagram('sprintuno_treArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctxservicearea', graph_attr=nodeattr):
+          wis=Custom('wis','./qakicons/symActorSmall.png')
+          oprobot=Custom('oprobot','./qakicons/symActorSmall.png')
+          incinerator=Custom('incinerator','./qakicons/symActorSmall.png')
+     with Cluster('ctxbasicrobot', graph_attr=nodeattr):
+          basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
+     wis >> Edge( label='startIncinerator', **eventedgeattr, decorate='true', fontcolor='red') >> sys
+     sys >> Edge( label='endBurn', **evattr, decorate='true', fontcolor='darkgreen') >> oprobot
+     oprobot >> Edge( label='startBurn', **eventedgeattr, decorate='true', fontcolor='red') >> sys
+     sys >> Edge( label='startIncinerator', **evattr, decorate='true', fontcolor='darkgreen') >> incinerator
+     sys >> Edge( label='startBurn', **evattr, decorate='true', fontcolor='darkgreen') >> incinerator
+     incinerator >> Edge( label='endBurn', **eventedgeattr, decorate='true', fontcolor='red') >> sys
+     oprobot >> Edge(color='magenta', style='solid', decorate='true', label='<engage &nbsp; moverobot<font color="darkgreen"> moverobotdone moverobotfailed</font> &nbsp; >',  fontcolor='magenta') >> basicrobot
+     oprobot >> Edge(color='blue', style='solid',  decorate='true', label='<info &nbsp; >',  fontcolor='blue') >> wis
+     wis >> Edge(color='blue', style='solid',  decorate='true', label='<robotStart &nbsp; >',  fontcolor='blue') >> oprobot
+diag
--- sprint3/src/main/java/test/GlobalTest.java
+++ sprint3/src/main/java/test/GlobalTest.java
@@ -13,82 +13,54 @@ import org.junit.Test;
 
 public class GlobalTest {
 
-	private static final List<String> containerIds = new ArrayList<>();
-	private static final String[] containerConfigs = { 
-			"sprint2:sprint2:latest:8021:8021", // Image, with port mapping
-			"basicrobot24:docker.io/natbodocker/basicrobot24:1.0:8020:8020", // Image, with port mapping
-			"sprint1:sprint1:latest:8022:8022", // Image, with port mapping
-			"venv:docker.io/natbodocker/virtualrobotdisi23:1.0:8090:8091" // Image, with multiple port mappings
-	};
+    private static final String DOCKER_COMPOSE_FILE = "/home/andrea/unibo/iss/TemaFinale2024_ISS/docker-compose.yml";
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		for (String config : containerConfigs) {
-			String containerId = startContainer(config);
-			System.out.println("launched config "+ config + " " + containerId);
-			if (containerId != null) {
-				containerIds.add(config.split(":")[0]);
-			}
-		}
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        executeCommand("docker-compose -f " + DOCKER_COMPOSE_FILE + " up -d");
+    }
 
-	private static String startContainer(String config) throws Exception {
-		String[] parts = config.split(":");
-		String name = parts[0];
-		String image = parts[1];
-		String version=parts[2];
-		String hostPort = parts[3];
-		String containerPort = parts[4];
+    @Test
+    public void testContainersRunning() throws Exception {
+        List<String> containerIds = getRunningContainerIds();
+        for (String containerId : containerIds) {
+            System.out.println("testing " + containerId);
+            assertTrue("Container is not running: " + containerId, containerId != null);
+        }
+    }
 
-		// Costruisci il comando docker run per un solo port mapping
-		String command = String.format("docker run -d --name %s --rm -p %s:%s %s:%s", name, hostPort, containerPort, image, version);
-		System.out.println(command);
-		// Esegui il comando
-		return executeCommand(command);
-	}
+    private static List<String> getRunningContainerIds() throws Exception {
+        List<String> containerIds = new ArrayList<>();
+        String command = "docker ps -q";
+        String result = executeCommand(command);
+        for (String line : result.split("\n")) {
+            containerIds.add(line.trim());
+        }
+        return containerIds;
+    }
 
-	private static void stopAndRemoveContainer(String containerId) throws Exception {
-		executeCommand("docker stop " + containerId);
-//		executeCommand("docker rm " + containerId);
-	}
+    private static String executeCommand(String command) throws Exception {
+        Process process = new ProcessBuilder().command(command.split(" ")).redirectErrorStream(true).start();
 
-	private static String executeCommand(String command) throws Exception {
-		Process process = new ProcessBuilder().command(command.split(" ")).redirectErrorStream(true).start();
+        StringBuilder output = new StringBuilder();
 
-		StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+        }
 
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				output.append(line).append("\n");
-			}
-		}
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new RuntimeException("Command failed: " + command + "\nOutput: " + output);
+        }
 
-		int exitCode = process.waitFor();
-		if (exitCode != 0) {
-			throw new RuntimeException("Command failed: " + command + "\nOutput: " + output);
-		}
+        return output.toString().trim();
+    }
 
-		return output.toString().trim();
-	}
-
-	@Test
-	public void testContainersRunning() throws Exception {
-		
-	    for (String containerId : containerIds) {
-	        String command = "docker ps -q --filter name=" + containerId; // Corrected line
-	        String result = executeCommand(command);
-	        System.out.println("testing "+containerId);
-	        assertTrue("Container is not running: " + containerId, result!=null);
-	    }
-	}
-	
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("killing all");
-		for (String containerId : containerIds) {
-			System.out.println("killing "+containerId);
-			stopAndRemoveContainer(containerId);
-		}
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        executeCommand("docker-compose -f " + DOCKER_COMPOSE_FILE + " down");
+    }
 }
